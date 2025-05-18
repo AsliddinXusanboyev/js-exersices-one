b250bdeecc752ad399d34071de1c443d2207bae9
sabitkhanov <musaxanovichbositxon@gmail.com>
2025-05-18 21:29:33 +0500

    if else

---
+++ index.js
@@ -0,0 +1,37 @@
+// let yosh = 17;
+
+// if (yosh >= 18) {
+//   console.log("Siz balog'at yoshidasiz.");
+// } else {
+//   console.log("Siz balog'at yoshida emassiz.");
+// }
+
+// let a = "bozor";
+
+// if (a == "maktab hovlisi") {
+//   console.log("siz maktab hovlisi ichidasiz");
+// } else {
+//   console.log("siz maktab hovlisi ichida emassiz");
+// }
+
+// let a = "uy";
+// if (a == "uy") {
+//   console.log("siz uydasiz");
+// } else {
+//   console.log("siz uyda emassiz");
+// }
+
+// let a = "20";
+// if (a == "20") console.log("a teng 20 ga ");
+// else console.log("a teng emas 20 ga");
+
+// let a = 20;
+// natija = a == 18 ? "siz 18 yoshadisz" : "siz 18 yoshdan kichiksiz";
+// console.log(natija);
+
+// let title = "Webrain academy";
+// if (title == "Webbrain Academy") {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
